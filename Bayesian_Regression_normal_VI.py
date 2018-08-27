1c17537e9104f1ab743dc3bef692f34cf8db9d43
kousathanas <kousathanas.a@gmail.com>
2018-08-27 16:57:29 +0200

    Training and test sets

--- Bayesian_Regression_normal_VI.py
+++ Bayesian_Regression_normal_VI.py
@@ -1,10 +1,12 @@
-#Athanasios Kousathanas
-#Simple linear Bayesian regression with Edward/tensorflow probabilistic framework
+#Bayesian regression with Edward/tensorflow probabilistic framework
+#Uses Variational Inference (VI)
+#Assumes normal prior for effect sizes.
 
 import numpy as np
 import matplotlib.pyplot as plt
 import edward as ed
 import tensorflow as tf
+import matplotlib.backends.backend_pdf
 
 from edward.models import Normal
 
@@ -21,14 +23,16 @@ true_sigma_b=2
 true_sigma_e=1
 
 #covariates true_beta of dimension M
-true_beta=np.random.normal(1.0, true_sigma_b, size=M)
-#noise epsilon of dimension N
-epsilon=np.random.normal(0.0, true_sigma_e, size=N)
+true_beta=np.random.normal(0.0, true_sigma_b, size=M)
 
-#genotype matrix X_train
+#genotype matrices X_train and X_test, for training and test data
 X_train = np.random.normal(0.0, 1.0, size=(N, M))
-#Phenotypes Y_train
-Y_train = np.dot(X_train, true_beta) + true_alpha + epsilon
+X_test = np.random.normal(0.0, 1.0, size=(N, M))
+#Phenotypes Y_train and Y_test for training and test data
+Y_train = np.dot(X_train, true_beta) + true_alpha + np.random.normal(0.0, true_sigma_e, size=N)
+Y_test =np.dot(X_test, true_beta) + true_alpha + np.random.normal(0.0, true_sigma_e, size=N)
+
+tf.reset_default_graph()
 
 #Define Bayesian regression model
 X = tf.placeholder(tf.float32, [N, M])
@@ -51,22 +55,31 @@ inference = ed.KLqp({beta: qbeta, alpha: qalpha}, data={X: X_train, Y: Y_train})
 inference.run(n_samples=10, n_iter=10000)
 
 sess = ed.get_session()
+
 #obtain posterior mean for beta and alpha
 Ebeta=sess.run(qbeta.mean())
 Ealpha=sess.run(qalpha.mean())
 
-plt.scatter(Ebeta,true_beta)
+figpdf = matplotlib.backends.backend_pdf.PdfPages("Bayesian_Regression_normal_VI.pdf")
+#plot estimated vs. true betas
+plt.scatter(Ebeta, true_beta)
 plt.title('Estimated versus true betas')
 plt.xlabel('Estimated')
 plt.ylabel('True')
+plt.savefig(figpdf,format='pdf')
+plt.close()
 
-Y_est=np.dot(X_train,Ebeta)+Ealpha
-plt.scatter(Y_est,Y_train)
-plt.title('Estimated versus true Y')
+#plot predicted phenotypes vs. phenotypes from a test dataset
+Y_est=np.dot(X_test,Ebeta)+Ealpha
+plt.scatter(Y_est,Y_test)
+plt.title('Estimated versus test Y'+', R2= '+str(round(np.corrcoef(Y_est,Y_test)[0][1]**2,3)))
 plt.xlabel('Estimated')
 plt.ylabel('True')
+plt.savefig(figpdf, format='pdf')
+plt.close()
 
+figpdf.close()
 #Rest of estimated parameters
-print("E(alpha)=",Ealpha,",","True=",true_alpha)
-print("E(sigma_e)=",sigma_e.eval(),",","True=",true_sigma_e)
-print("E(sigma_b)=",sigma_b.eval(),",","True=",true_sigma_b)
+print("E(alpha)=", Ealpha, ",", "True=",true_alpha)
+print("E(sigma_e)=", sigma_e.eval(), ",", "True=", true_sigma_e)
+print("E(sigma_b)=", sigma_b.eval(), ",", "True=", true_sigma_b)
